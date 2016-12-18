371ff37772e436e0809b701dd0e82e4bd5247786
geoffrey wall <wallge@gmail.com>
2016-12-17 17:15:10 -0800

    added more error checking and also included self testing mode

--- c++/primeFactors/main.cpp
+++ c++/primeFactors/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <climits>
+#include <cstdlib>
 #include "primeFactorComputer.h"
 
 //generate numTests random numbers in the range 1 to INT_MAX and plug them into our primeFactorComputer class
@@ -9,10 +10,13 @@
 //this is only used for testing purposes and is not part of the main program
 int testPrimeFactors(int numTests) {
 
+    if (numTests < 1)
+        return -1;
+
     //setup the random number generator
     std::mt19937 rng;
     rng.seed(std::random_device()());
-    std::uniform_int_distribution<std::mt19937::result_type> uniform(1, INT_MAX);
+    std::uniform_int_distribution<std::mt19937::result_type> uniform(2, INT_MAX);
 
     int i = 0;
     //loop until we compute all the tests
@@ -23,36 +27,58 @@ int testPrimeFactors(int numTests) {
         int computed = testPrimeFactors.evaluatePrimeFactors();
         bool allElementsArePrime = testPrimeFactors.testPrimality();
 
+        std::cout << testInput << std::endl;
+        testPrimeFactors.printPrimeFactors();
+
         if (computed != testInput || !allElementsArePrime) {
-            std::cout << "input = " << testInput << " computed product = " << computed << " allElementsArePrime = " << allElementsArePrime << std::endl;
+            std::cout << "*****************************************************************\n";
+            std::cout << "ERROR input = " << testInput << " computed product = " << computed << " allElementsArePrime = " << allElementsArePrime << std::endl;
             return -1;
         }
+
         i++;
     }
 
     return 0;
 }
 
-int main() {
 
-    unsigned long i;
+//If you run the program with no arguments, it runs as specified in the assignment description,
+//if you run the program with a single argument that is a positive integer, it will run in test mode.
+//Test mode will generate random positive integers (the number of which is specified by the command line argument)
+//and then run them through the prime factors algorithm and check the answers for correctness.
+int main(int argc, char **argv) {
 
-    while(1) {
-        //executes loop if the input fails (e.g., no characters were read)
-        while (std::cout << "Please enter an integer.\n" && !(std::cin >> i)) {
-            std::cin.clear(); //clear bad input flag
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
-            std::cout << "Invalid input. Exiting." << std::endl;
-            return -1;
+    if ( argc >= 2 ) {
+        std::cout << argv[0] << ' ' << argv[1] << std::endl;
+        int i = atoi(argv[1]);
+        testPrimeFactors(i);
+    } else {
+
+        signed long i;
+
+        while (1) {
+            //executes loop if the input fails (e.g., no characters were read)
+            while (std::cout << "Please enter an integer.\n" && !(std::cin >> i)) {
+                std::cin.clear(); //clear bad input flag
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+                std::cout << "Invalid input. Exiting." << std::endl;
+                return -1;
+            }
+
+            if (i > INT_MAX) {
+                std::cout << "Input exceeds maximum int32 value: " << INT_MAX << ". Exiting." << std::endl;
+                return -1;
+            }
+
+            if (i < 2) {
+                std::cout << "Only positive values larger than 1 are permitted. Exiting." << std::endl;
+                return -1;
+            }
+
+            primeFactorComputer testPrimeFactors((int) i);
+            testPrimeFactors.printPrimeFactors();
         }
-
-        if (i > INT_MAX) {
-            std::cout << "Input exceeds maximum int32 value: " << INT_MAX << ". Exiting." << std::endl;
-            return -1;
-        }
-
-        primeFactorComputer testPrimeFactors((int)i);
-        testPrimeFactors.printPrimeFactors();
     }
 
     return 0;
